c433bf835f11a2edad76613fca0d7a023bb3f0b4
Andrew <krciga22@gmail.com>
2023-11-11 23:18:55 -0500

    [server] Setup Database Connection

---
+++ AgileStudioServer/DBContext.cs
@@ -0,0 +1,13 @@
+﻿using AgileStudioServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgileStudioServer
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions contextOptions) : base(contextOptions)
+        {
+
+        }
+    }
+}
--- AgileStudioServer/Program.cs
+++ AgileStudioServer/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace AgileStudioServer
 {
     public class Program
@@ -12,6 +14,25 @@ namespace AgileStudioServer
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DBContext>(optionsBuilder => {
+                var dbHost = builder.Configuration.GetValue<string>("DB:Host");
+                var dbName = builder.Configuration.GetValue<string>("DB:Name");
+                var dbUser = builder.Configuration.GetValue<string>("DB:User");
+                var dbPass = builder.Configuration.GetValue<string>("DB:Pass");
+
+                var connectionString = String.Format("server={0};database={1};user={2};password={3}", dbHost, dbName, dbUser, dbPass);
+                var mysqlVersion = new Version("8.0");
+                optionsBuilder
+                    .UseMySql(
+                        connectionString,
+                        MySqlServerVersion.Create(
+                            mysqlVersion,
+                            Pomelo.EntityFrameworkCore.MySql.Infrastructure.ServerType.MySql
+                        )
+                    )
+                    .UseSnakeCaseNamingConvention();
+            });
 
             var app = builder.Build();
 
