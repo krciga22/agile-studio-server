840d1d13831cb3a86d3c916f9d5ef31697007b26
Andrew <krciga22@gmail.com>
2023-11-12 16:56:49 -0500

    [server] Add DBContext for Dependency Injection in Unit Tests

---
+++ AgileStudioServerTest/Startup.cs
@@ -0,0 +1,38 @@
+﻿using AgileStudioServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+[assembly: UserSecretsId("458c7384-b3de-4f95-8b07-128aa520c896")]
+namespace AgileStudioServerTest
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder().AddUserSecrets(typeof(Startup).GetTypeInfo().Assembly);
+            var configuration = builder.Build();
+
+            services.AddDbContext<DBContext>(optionsBuilder => {
+                var dbHost = configuration.GetValue<string>("DB:Host");
+                var dbName = configuration.GetValue<string>("DB:Name");
+                var dbUser = configuration.GetValue<string>("DB:User");
+                var dbPass = configuration.GetValue<string>("DB:Pass");
+
+                var connectionString = String.Format("server={0};database={1};user={2};password={3}", dbHost, dbName, dbUser, dbPass);
+                var mysqlVersion = new Version("8.0");
+                optionsBuilder
+                    .UseMySql(
+                        connectionString,
+                        MySqlServerVersion.Create(
+                            mysqlVersion,
+                            Pomelo.EntityFrameworkCore.MySql.Infrastructure.ServerType.MySql
+                        )
+                    )
+                    .UseSnakeCaseNamingConvention();
+            });
+        }
+    }
+}
