90f1f56b41f91ed5458fb906cf7b737c768753a8
Andrew <krciga22@gmail.com>
2024-02-04 00:14:10 -0500

    [server] Fix local variable name format

--- AgileStudioServer/Attributes/Validation/ValidBacklogItemTypeForBacklogItemPostDto.cs
+++ AgileStudioServer/Attributes/Validation/ValidBacklogItemTypeForBacklogItemPostDto.cs
@@ -19,19 +19,19 @@ namespace AgileStudioServer.Attributes.Validation
                 throw new ArgumentNullException(nameof(value));
             }
 
-            var _DBContext = (DBContext?)validationContext.GetService(typeof(DBContext)) ??
+            var dbContext = (DBContext?)validationContext.GetService(typeof(DBContext)) ??
                 throw new ServiceNotFoundException(nameof(DBContext));
 
             var dto = (BacklogItemPostDto)value;
 
-            var project = _DBContext.Project.Find(dto.ProjectId) ??
+            var project = dbContext.Project.Find(dto.ProjectId) ??
                 throw new EntityNotFoundException(nameof(Project), dto.ProjectId.ToString());
 
-            var backlogItemType = _DBContext.BacklogItemType.Find(dto.BacklogItemTypeId) ??
+            var backlogItemType = dbContext.BacklogItemType.Find(dto.BacklogItemTypeId) ??
                 throw new EntityNotFoundException(nameof(BacklogItemType), dto.BacklogItemTypeId.ToString());
 
-            _DBContext.Entry(project).Reference("BacklogItemTypeSchema").Load();
-            _DBContext.Entry(backlogItemType).Reference("BacklogItemTypeSchema").Load();
+            dbContext.Entry(project).Reference("BacklogItemTypeSchema").Load();
+            dbContext.Entry(backlogItemType).Reference("BacklogItemTypeSchema").Load();
 
             if (backlogItemType.BacklogItemTypeSchema.ID != project.BacklogItemTypeSchema.ID)
             {
