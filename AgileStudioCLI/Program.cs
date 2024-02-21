2819fe79dbef01eceda3c639450f59619ac56819
Andrew <krciga22@gmail.com>
2024-02-21 00:50:13 -0500

    [cli] Execute multiple cli commands and exit the cli when ready

--- AgileStudioCLI/Program.cs
+++ AgileStudioCLI/Program.cs
@@ -3,37 +3,37 @@ using AgileStudioCLI.Services;
 
 Console.WriteLine("Agile Studio CLI");
 Console.WriteLine("");
-
-// show a list of available commands
 Console.WriteLine("Available Commands:");
 Console.WriteLine("- ClearFixtures");
 Console.WriteLine("- LoadFixtures");
-Console.WriteLine("");
-
-// ask user to enter a command to execute
-Console.WriteLine("Enter a command to execute: ");
-var selectedCommand = Console.ReadLine();
+Console.WriteLine("- Exit | \\d");
 Console.WriteLine("");
 
 var dbContext = DBContextFactory.Create();
 
-// execute the selected command
-switch (selectedCommand)
+string[] exitCommands = new string[2];
+exitCommands[0] = "Exit";
+exitCommands[1] = "\\d";
+
+var selectedCommand = "";
+while(!exitCommands.Contains(selectedCommand))
 {
-    case "ClearFixtures":
-        new ClearFixtures(dbContext).Execute(null);
-        break;
+    selectedCommand = Console.ReadLine();
+    switch (selectedCommand)
+    {
+        case "ClearFixtures":
+            new ClearFixtures(dbContext).Execute(null);
+            break;
 
-    case "LoadFixtures":
-        new LoadFixturesCommand(dbContext).Execute(null);
-        break;
+        case "LoadFixtures":
+            new LoadFixturesCommand(dbContext).Execute(null);
+            break;
 
-    default:
-        Console.WriteLine("Command not found");
-        break;
+        default:
+            if (!exitCommands.Contains(selectedCommand))
+            {
+                Console.WriteLine("Command not found");
+            }
+            break;
+    }
 }
-
-// wait a few seconds before exiting
-Console.WriteLine("");
-Console.WriteLine("Exiting...");
-Thread.Sleep(2000);
