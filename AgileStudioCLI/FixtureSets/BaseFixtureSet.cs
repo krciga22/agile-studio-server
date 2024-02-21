f5cbf34ef05a789fc2ff22caf1b8c2f97352b680
Andrew <krciga22@gmail.com>
2024-02-21 00:28:16 -0500

    [cli] Load sprint, release, and backlog item fixtures

--- AgileStudioCLI/FixtureSets/BaseFixtureSet.cs
+++ AgileStudioCLI/FixtureSets/BaseFixtureSet.cs
@@ -7,10 +7,15 @@ namespace AgileStudioCLI.FixtureSets
     {
         public void LoadFixtures(DBContext dbContext)
         {
+            LoadAgileStudioProject(dbContext);
+        }
+
+        private void LoadAgileStudioProject(DBContext dbContext)
+        {
             var fixtures = new Fixtures(dbContext);
 
             var workflow = fixtures.CreateWorkflow("Story & Defect Workflow");
-            fixtures.CreateWorkflowState("In Backlog", workflow);
+            var workflowStateInBacklog = fixtures.CreateWorkflowState("In Backlog", workflow);
             fixtures.CreateWorkflowState("In Planning", workflow);
             fixtures.CreateWorkflowState("In Development", workflow);
             fixtures.CreateWorkflowState("In Testing", workflow);
@@ -18,10 +23,20 @@ namespace AgileStudioCLI.FixtureSets
             fixtures.CreateWorkflowState("Cancelled", workflow);
 
             var backlogItemTypeSchema = fixtures.CreateBacklogItemTypeSchema("Agile Studio Backlog Item Type Schema");
-            fixtures.CreateBacklogItemType("Story", backlogItemTypeSchema, workflow);
+            var backlogItemTypeStory = fixtures.CreateBacklogItemType("Story", backlogItemTypeSchema, workflow);
             fixtures.CreateBacklogItemType("Defect", backlogItemTypeSchema, workflow);
 
-            fixtures.CreateProject("Agile Studio", backlogItemTypeSchema);
+            var project = fixtures.CreateProject("Agile Studio", backlogItemTypeSchema);
+            var sprint1 = fixtures.CreateSprint(1, project);
+            var release1_0_0 = fixtures.CreateRelease("1.0.0", project);
+
+            fixtures.CreateBacklogItem(
+                title: "Test Story", 
+                project: project,
+                backlogItemType: backlogItemTypeStory,
+                workflowState: workflowStateInBacklog, 
+                sprint: sprint1, 
+                release: release1_0_0);
         }
     }
 }
